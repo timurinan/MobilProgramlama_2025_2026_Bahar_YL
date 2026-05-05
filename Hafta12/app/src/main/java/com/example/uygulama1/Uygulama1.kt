f088d849753ff8a530ddc2c55e3c1c34b9a0d3dd
timurinan <91524596+timurinan@users.noreply.github.com>
2026-05-05 11:05:26 +0300

    //

---
+++ Hafta12/app/src/main/java/com/example/uygulama1/Uygulama1.kt
@@ -0,0 +1,12 @@
+package com.example.uygulama1
+
+import java.util.Scanner
+
+fun main() {
+    println("Merhaba bu bir denemedir")
+
+    val girdi= Scanner(System.`in`)
+    var ad=girdi.next()
+
+    println("Girilen ad: $ad")
+}
